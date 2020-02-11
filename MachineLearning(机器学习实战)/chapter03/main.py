7565ea4f26105bf86fb8ff0d2a7d79c450be87b7
张富利 <>
2020-02-11 15:46:08 +0800

    no message

--- MachineLearning(机器学习实战)/chapter03/main.py
+++ MachineLearning(机器学习实战)/chapter03/main.py
@@ -1,5 +1,9 @@
 from chapter03.trees import *
+from chapter03.treePlotter import *
 
 if __name__ == '__main__':
     myDat, labels = createDataSet()
-    print(calcShaannonEnt(myDat))
+    print(calcShannonEnt(myDat))
+    print(splitDataSet(myDat, 0, 1))
+    myTree = retrieveTree(0)
+    createPlot(myTree)
---
+++ MachineLearning(机器学习实战)/chapter04/main.py
@@ -0,0 +1,10 @@
+from chapter04.bayes import *
+
+if __name__ == '__main__':
+    # listOPosts, listClasses = loadDataSet()
+    # myVocabList = createVocabList(listOPosts)
+    # trainMat = []
+    # for postinDoc in listOPosts:
+    #     trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    # p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+    testingNB()
